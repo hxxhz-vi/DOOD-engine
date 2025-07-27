a8b18ae74e19ef71798fefdaf483dad8d5a9f195
hhxz-vi <heorhiiiukrainian2006@proton.me>
2025-07-27 19:19:13 +0300

    1. CMakeLists.txt
    -Update project names
    
    -Disable CXX_MODULE_CMAKE_API for c++23
    
    -add find_package for sdl2
    
    2. include/config.hpp
    
    -Update include guard
    
    -Define WIDTH and HIEGHT macros for window
    
    -Include SDL2 and SDL_main headers
    
    3. src/main.cpp
    
    -Initialize sdl2 window and surface
    
    -Add window close event loop
    
    -Fill window with white background

--- include/config.hpp
+++ include/config.hpp
@@ -1,7 +1,15 @@
-#ifndef __CONFIG__H
-#define __CONFIG__H
+#ifndef CONFIG__HPP
+#define CONFIG__HPP
 
+#define SDL_MAIN_HANDLED
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 640
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_main.h"
 #include <cstdint>
 #include <iostream>
+#include <vector>
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,49 @@
 #include "config.hpp"
 
-int32_t
-main()
+struct AppState
 {
-  if (!std::cout)
-    return -1;
+  SDL_Window* window = nullptr;
+  SDL_Surface* surface = nullptr;
+};
+
+int32_t
+main (void)
+{
+  AppState app;
+
+  if (SDL_Init (SDL_INIT_VIDEO) < 0)
+    {
+      std::cout << "SDL init video error!\n" << SDL_GetError ();
+    }
   else
-    std::cout << "Fuck world!\n" << std::endl;
+    {
+      app.window = SDL_CreateWindow ("DOOD-engine", SDL_WINDOWPOS_UNDEFINED,
+                                     SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                     SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+      if (app.window == nullptr)
+        {
+          std::cout << "SDL cant create a window!\n" << SDL_GetError ();
+        }
+      else
+        {
+          app.surface = SDL_GetWindowSurface (app.window);
+          SDL_FillRect (app.surface, nullptr,
+                        SDL_MapRGB (app.surface->format, 0xFF, 0xFF, 0xFF));
+          SDL_UpdateWindowSurface (app.window);
+        }
+      SDL_Event e;
+      bool quit = false;
+      while (quit == false)
+        {
+          while (SDL_PollEvent (&e))
+            {
+              if (e.type == SDL_QUIT)
+                quit = true;
+            }
+        }
+    }
+  SDL_DestroyWindow (app.window);
+  SDL_Quit ();
+
   return 0;
 }
